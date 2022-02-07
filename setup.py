5ffb0f5f579a12e69d9cf4a9096a81cf40562ac1
qzmlgfj <qzmlgfj@gmail.com>
2022-02-07 13:03:02 +0800

    Modify some require packages in setup.py

--- setup.py
+++ setup.py
@@ -3,16 +3,18 @@ from setuptools import setup, find_packages
 
 setup(
     name='ant_net_monitor',
-    version='0.0.1',
+    version='0.0.3',
     packages=find_packages(),
     include_package_data=True,
     zip_safe=False,
     install_requires=[
-        'flask',
-        'flask-sqlalchemy',
-        'flask_cors',
-        'click',
-        'psutil',
-        'dataclasses',
-    ]
+        'flask>=2.0.2',
+        'flask-sqlalchemy>=2.5.1',
+        'flask_cors>=3.0.10',
+        'click>=8.0.3',
+        'psutil>=5.9.0',
+        'dataclasses>=0.6',
+    ],
+    long_description_content_type='text/markdown',
+    long_description=open('README.md').read(),
 )
