09a433943ebc5013d18f90034c480819c25de00f
qzmlgfj <qzmlgfj@gmail.com>
2022-03-05 21:24:33 +0800

    Add some Jest tests

--- frontend/tests/unit/example.spec.js
+++ frontend/tests/unit/example.spec.js
@@ -1,9 +1,11 @@
 import { mount } from '@vue/test-utils'
 import HeadBar from '@/components/HeadBar.vue'
+import { NH1, NButton } from 'naive-ui'
 
 describe('HeadBar.vue', () => {
   it('renders HeadBar when passed', () => {
     const wrapper = mount(HeadBar)
-    console.log(wrapper.contains('h1'))
+    expect(wrapper.findComponent(NH1).text()).toBe('Ant Net Monitor')
+    expect(wrapper.findComponent(NButton).text()).toBe('Change')
   })
 })
