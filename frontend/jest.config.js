26f52000b285c840b8883bd69127b081babfb9ae
qzmlgfj <qzmlgfj@gmail.com>
2022-03-05 17:57:01 +0800

    Add some jest tests

--- frontend/jest.config.js
+++ frontend/jest.config.js
@@ -1,5 +1,6 @@
 module.exports = {
   preset: '@vue/cli-plugin-unit-jest',
+  collectCoverage: true,
   transform: {
     '^.+\\.vue$': 'vue-jest'
   }
--- frontend/tests/unit/example.spec.js
+++ frontend/tests/unit/example.spec.js
@@ -1,12 +1,9 @@
-import { shallowMount } from '@vue/test-utils'
-import HelloWorld from '@/components/HelloWorld.vue'
+import { mount } from '@vue/test-utils'
+import HeadBar from '@/components/HeadBar.vue'
 
-describe('HelloWorld.vue', () => {
-  it('renders props.msg when passed', () => {
-    const msg = 'new message'
-    const wrapper = shallowMount(HelloWorld, {
-      props: { msg }
-    })
-    expect(wrapper.text()).toMatch(msg)
+describe('HeadBar.vue', () => {
+  it('renders HeadBar when passed', () => {
+    const wrapper = mount(HeadBar)
+    console.log(wrapper.contains('h1'))
   })
 })
