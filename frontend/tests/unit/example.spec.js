dff740965ab1067264a2e10a5ddb005e218a134b
qzmlgfj <qzmlgfj@gmail.com>
2022-03-06 17:44:59 +0800

    Add a Jest test to check if event emits.

--- frontend/tests/unit/example.spec.js
+++ frontend/tests/unit/example.spec.js
@@ -1,16 +1,22 @@
 import { mount } from '@vue/test-utils'
+import { createRouter, createWebHistory } from 'vue-router'
+import { NSpace, NH1, NButton } from 'naive-ui'
+
+import { routes } from '@/router'
+import store from '@/utils/store'
+
 import HeadBar from '@/components/HeadBar.vue'
 import SideBar from '@/components/SideBar.vue'
-import { routes } from '@/router'
-import { createRouter, createWebHistory } from 'vue-router'
-import { NSpace,NH1, NButton } from 'naive-ui'
 
 describe('HeadBar.vue', () => {
-    it('renders HeadBar when passed', () => {
+    test('renders HeadBar when passed', async () => {
         const wrapper = mount(HeadBar)
         expect(wrapper.findComponent(NSpace).exists()).toBe(true)
         expect(wrapper.findComponent(NH1).text()).toBe('Ant Net Monitor')
         expect(wrapper.findComponent(NButton).text()).toBe('Change')
+
+        wrapper.findComponent(NButton).trigger('click')
+        expect(wrapper.emitted().hasOwnProperty('changeTheme')).toBe(true)
     })
 })
 
@@ -26,12 +32,16 @@ beforeEach(async () => {
     await router.isReady()
 });
 
-test('SideBar.vue', async () => {
-    const wrapper = mount(SideBar, {
-        global: {
-            plugins: [router]
-        }
-    })
+describe('SideBar.vue', () => {
+    test('renders SideBar when passed', async () => {
+        const wrapper = mount(SideBar, {
+            global: {
+                plugins: [router]
+            }
+        })
 
-    console.log(wrapper.find('#sidebar .n-menu-item a').attributes('href'))
+        console.log(wrapper.find('#sidebar .n-menu-item a').attributes('href'))
+    })
 })
+
+
