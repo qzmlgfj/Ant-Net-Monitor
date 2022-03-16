9121bc19991b9129cbb738007e288658ce603fb1
qzmlgfj <qzmlgfj@gmail.com>
2022-03-16 16:43:02 +0800

    Using scoped_session to support multithread

--- ant_net_monitor/threads.py
+++ ant_net_monitor/threads.py
@@ -1,48 +1,64 @@
 import threading
 from time import sleep
 
-from ant_net_monitor.status.cpu_status import CPUStatus, save_cpu_status
-from ant_net_monitor.status.ram_status import RAMStatus, save_ram_status
-from ant_net_monitor.status.basic_status import BasicStatus, save_basic_status
+from sqlalchemy.orm import scoped_session, sessionmaker
 
-# TODO 测试情况下的环境变量
-def set_basic_status_thread(app):
+from .extensions import db
+from .status.basic_status import BasicStatus, save_basic_status
+from .status.cpu_status import CPUStatus, save_cpu_status
+from .status.ram_status import RAMStatus, save_ram_status
+
+# TODO 整个函数变量进去，进一步封装
+
+
+def set_basic_status_thread(session_factory, app):
     """Register basic status thread."""
 
     def save_status_loop(app):
         with app.app_context():
             while True:
-                save_basic_status(BasicStatus())
+                save_basic_status(thread_session, BasicStatus())
 
+    thread_session = scoped_session(session_factory)
     save_status_thread = threading.Thread(target=save_status_loop, args=(app,))
     save_status_thread.start()
 
 
-def set_cpu_status_thread(app):
+def set_cpu_status_thread(session_factory, app):
     """Register cpu status thread."""
 
     def save_status_loop(app):
         with app.app_context():
             while True:
-                save_cpu_status(CPUStatus())
+                save_cpu_status(thread_session, CPUStatus())
 
+    thread_session = scoped_session(session_factory)
     save_status_thread = threading.Thread(target=save_status_loop, args=(app,))
     save_status_thread.start()
 
 
-def set_ram_status_thread(app):
+def set_ram_status_thread(session_factory, app):
     """Register ram status thread."""
 
     def save_status_loop(app):
         with app.app_context():
             while True:
-                save_ram_status(RAMStatus())
+                save_ram_status(thread_session, RAMStatus())
                 sleep(1)
 
+    thread_session = scoped_session(session_factory)
     save_status_thread = threading.Thread(target=save_status_loop, args=(app,))
     save_status_thread.start()
 
+
+def set_session_factory(app):
+    with app.app_context():
+        engine = db.get_engine()
+        return sessionmaker(bind=engine)
+
+
 def set_all_threads(app):
-    set_basic_status_thread(app)
-    set_cpu_status_thread(app)
-    set_ram_status_thread(app)
+    session_factory = set_session_factory(app)
+    set_basic_status_thread(session_factory, app)
+    set_cpu_status_thread(session_factory, app)
+    set_ram_status_thread(session_factory, app)
