95fa9631b1ef2b0e2b9d47180230a141b77a4844
qzmlgfj <qzmlgfj@gmail.com>
2022-03-21 15:41:04 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='ant_net_monitor',
-    version='0.1.1dev1',
+    version='0.1.1dev2',
     packages=find_packages(),
     include_package_data=True,
     zip_safe=False,
