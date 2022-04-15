7f8ac33d6ad7bd3a38875db3d61e78c2aa46a3bc
qzmlgfj <qzmlgfj@gmail.com>
2022-04-15 17:34:51 +0800

    Refactor series.js to generate series automatically

--- frontend/src/components/Info.vue
+++ frontend/src/components/Info.vue
@@ -24,10 +24,7 @@ import {
     updateLineChart,
     getHistoryLineChart,
 } from "@/utils/request";
-import { setCPUSeries, updateCPUSeries } from "@/utils/series/cpu-series";
-import { setRAMSeries, updateRAMSeries } from "@/utils/series/ram-series";
-import { setDiskSeries, updateDiskSeries } from "@/utils/series/disk-series";
-import { setNetworkSeries, updateNetworkSeries } from "@/utils/series/network-series";
+import { initSeries, setSeries, updateSeries } from "@/utils/series";
 
 export default {
     name: "Info",
@@ -48,40 +45,15 @@ export default {
         };
     },
     methods: {
-        updateSeries(url) {
+        updateChart(url) {
             updateLineChart(url).then((response) => {
-                switch (this.$route.name) {
-                    case "CPU-Info":
-                        updateCPUSeries(response.data);
-                        break;
-                    case "RAM-Info":
-                        updateRAMSeries(response.data);
-                        break;
-                    case "Disk-Info":
-                        updateDiskSeries(response.data);
-                        break;
-                    case "Network-Info":
-                        updateNetworkSeries(response.data);
-                        break;
-                }
+                updateSeries(response.data);
             });
         },
-        initSeries(url) {
+        initChart(url) {
             initLineChart(url).then((response) => {
-                switch (this.$route.name) {
-                    case "CPU-Info":
-                        this.series = setCPUSeries(response.data);
-                        break;
-                    case "RAM-Info":
-                        this.series = setRAMSeries(response.data);
-                        break;
-                    case "Disk-Info":
-                        this.series = setDiskSeries(response.data);
-                        break;
-                    case "Network-Info":
-                        this.series = setNetworkSeries(response.data);
-                        break;
-                }
+                initSeries(response.data[0]);
+                this.series = setSeries(response.data);
             });
         },
         switchToHistory() {
@@ -90,20 +62,7 @@ export default {
                 clearInterval(this.interval);
                 getHistoryLineChart(this.$route.meta.apiUrl).then(
                     (response) => {
-                        switch (this.$route.name) {
-                            case "CPU-Info":
-                                this.series = setCPUSeries(response.data);
-                                break;
-                            case "RAM-Info":
-                                this.series = setRAMSeries(response.data);
-                                break;
-                            case "Disk-Info":
-                                this.series = setDiskSeries(response.data);
-                                break;
-                            case "Network-Info":
-                                this.series = setNetworkSeries(response.data);
-                                break;
-                        }
+                        this.series = setSeries(response.data);
                     }
                 );
             }
@@ -111,9 +70,9 @@ export default {
         switchToRealTime() {
             if (this.enableZoom) {
                 this.enableZoom = false;
-                this.initSeries(this.$route.meta.apiUrl);
+                this.initChart(this.$route.meta.apiUrl);
                 this.interval = setInterval(() => {
-                    this.updateSeries(this.$route.meta.apiUrl);
+                    this.updateChart(this.$route.meta.apiUrl);
                 }, this.intervalTime);
             }
         },
@@ -122,16 +81,16 @@ export default {
         },
     },
     created() {
-        this.initSeries(this.$route.meta.apiUrl);
+        this.initChart(this.$route.meta.apiUrl);
         this.interval = setInterval(() => {
-            this.updateSeries(this.$route.meta.apiUrl);
+            this.updateChart(this.$route.meta.apiUrl);
         }, this.intervalTime);
     },
     beforeRouteUpdate(to) {
         clearInterval(this.interval);
-        this.initSeries(to.meta.apiUrl);
+        this.initChart(to.meta.apiUrl);
         this.interval = setInterval(() => {
-            this.updateSeries(to.meta.apiUrl);
+            this.updateChart(to.meta.apiUrl);
         }, this.intervalTime);
         this.enableZoom = false;
         this.historyMode = false;
---
+++ frontend/src/utils/series.js
@@ -0,0 +1,86 @@
+import { ref } from "vue";
+
+/**
+ * ECharts需要的数据格式
+ */
+let series = [];
+/**
+ * 作为名称映射，用于将后端传回数据字段的名称转换为变量名
+ */
+let seriesDict = {};
+
+/**
+ * 
+ * @param {string} name 
+ * @returns {Object} 单个Series条目
+ */
+function initSingleObject(name) {
+    return {
+        name: name,
+        type: "line",
+        smooth: false,
+        symbol: "none",
+        areaStyle: {},
+        data: ref([]),
+    };
+}
+
+/**
+ * 初始化Series，不包含具体数据
+ * @param {Object} data，单条数据，用于解析并生成对应的条目
+ */
+function initSeries(data) {
+    series = [];
+    seriesDict = {};
+
+    for (const name of Object.keys(data)) {
+        if (name === "time_stamp" || name === "id") {
+            continue;
+        }
+        let singleObject = initSingleObject(name);
+        series.push(singleObject);
+        seriesDict[name] = singleObject;
+    }
+}
+
+/**
+ * 装填数据
+ * @param {Object} data 完整的数据
+ * @returns {Object} 完整的Series
+ */
+function setSeries(data) {
+    const clearSeries = () => {
+        series.forEach(item => {
+            item.data.value = [];
+        });
+    }
+
+    clearSeries();
+
+    data.forEach(item => {
+        for (const name of Object.keys(item)) {
+            if (name === "time_stamp" || name === "id") {
+                continue;
+            }
+            seriesDict[name].data.value.push([new Date(item["time_stamp"]), item[name]]);
+        }
+    })
+
+    return series;
+}
+
+/**
+ * 更新数据
+ * @param {Object} data 实时传回的单条数据
+ */
+function updateSeries(data) {
+    for (const name of Object.keys(data)) {
+        if (name === "time_stamp" || name === "id") {
+            continue;
+        }
+        seriesDict[name].data.value.shift();
+        seriesDict[name].data.value.push([new Date(data["time_stamp"]), data[name]]);
+    }
+}
+
+export { initSeries, setSeries, updateSeries };
