f2b5813c03cafbd2aaf59ca208eaa1868c79055d
qzmlgfj <qzmlgfj@gmail.com>
2022-04-21 18:01:58 +0800

    Change n-layout to n-scrollbar to fix a issue causes incorrect color in dark mode

--- frontend/src/components/Overview.vue
+++ frontend/src/components/Overview.vue
@@ -2,25 +2,24 @@
     <div id="overview">
         <n-space wrap="false" size="large" justify="space-between">
             <n-card hoverable>
-                <n-layout
-                    :native-scrollbar="false"
+                <n-scrollbar
                     style="height: 35vh"
                 >
                     <alarm-time-line />
-                </n-layout>
+                </n-scrollbar>
             </n-card>
         </n-space>
     </div>
 </template>
 
 <script>
-import { NCard, NLayout } from "naive-ui";
+import { NCard, NScrollbar } from "naive-ui";
 import AlarmTimeLine from "@/components/overview/AlarmTimeLine.vue";
 
 export default {
     components: {
         NCard,
-        NLayout,
+        NScrollbar,
         AlarmTimeLine,
     },
 };
