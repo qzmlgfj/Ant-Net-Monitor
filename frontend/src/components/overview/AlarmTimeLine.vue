7c5f5f5fde6e95db4d50f9abc3746d3932941328
qzmlgfj <qzmlgfj@gmail.com>
2022-04-26 22:22:17 +0800

    Add NEmpty component to alarm time line

--- frontend/src/components/overview/AlarmTimeLine.vue
+++ frontend/src/components/overview/AlarmTimeLine.vue
@@ -1,33 +1,39 @@
 <script>
-import { NTimeline, NTimelineItem, NText, NTime } from "naive-ui";
+import { NTimeline, NTimelineItem, NText, NTime, NEmpty } from "naive-ui";
 import { h } from "vue";
 
 import { getAlarmLog } from "@/utils/request";
 
 export default {
     render() {
-        return h(
-            NTimeline,
-            {
-                size: "large",
-            },
-            this.alarmLog.map((item) => {
-                return h(NTimelineItem, {
-                    title: h(
-                        NText,
-                        { code: true },
-                        {
-                            default: () => item.name,
-                        }
-                    ),
-                    type: item.status === "warning" ? "warning" : "success",
-                    time: h(NTime, {
-                        type: "datetime",
-                        time: new Date(item.time_stamp),
-                    }),
-                });
-            })
-        );
+        if (this.alarmLog.length === 0) {
+            return h(NEmpty, {
+                description: "无警报记录",
+            });
+        } else {
+            return h(
+                NTimeline,
+                {
+                    size: "large",
+                },
+                this.alarmLog.map((item) => {
+                    return h(NTimelineItem, {
+                        title: h(
+                            NText,
+                            { code: true },
+                            {
+                                default: () => item.name,
+                            }
+                        ),
+                        type: item.status === "warning" ? "warning" : "success",
+                        time: h(NTime, {
+                            type: "datetime",
+                            time: new Date(item.time_stamp),
+                        }),
+                    });
+                })
+            );
+        }
     },
     data() {
         return {
